1c1fe8b785ae77179ae7850a45a5a3a4a9347a67
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:02:35 -0700

    Add files via upload
    Q 04 Node.js of 45 Assignments

---
+++ Q4.js
@@ -0,0 +1,7 @@
+"use strict";
+// 4. Famous Quotes: Find a quote form a famous person you admire. Print the quote and 
+// the name of its author. Your output should look something like the following, including the quatation marks:
+// Albert Einstein once said, "A person who never made a mistake never tried anything new."
+let name1 = ("Hazrat ALI said");
+console.log(`${name1} "Each Person's Grief is Accourding to His Courage"
+`);
---
+++ Q4.ts
@@ -0,0 +1,9 @@
+// 4. Famous Quotes: Find a quote form a famous person you admire. Print the quote and 
+// the name of its author. Your output should look something like the following, including the quatation marks:
+// Albert Einstein once said, "A person who never made a mistake never tried anything new."
+
+
+let name1: string = ("Hazrat ALI said")
+
+console.log(`${name1} "Each Person's Grief is Accourding to His Courage"
+`)
